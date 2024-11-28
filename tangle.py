7a4720c4b72ef3b95630eded0e1779fba3c6a416
Joshiepillow <joshbce123@gmail.com>
2024-11-28 08:52:15 -0600

    add code

--- tangle.py
+++ tangle.py
@@ -1,21 +1,4 @@
 import subprocess
-
-def generate_k4(a, b, c, d, e, f):
-    (e01, e02, e03, e12, e13, e23) = (((0, 1), a), ((0, 2), b), ((0, 3), c), ((1, 2), d), ((1, 3), e), ((2, 3), f))
-    return [[e01, e02, e03], [e01, e13, e12], [e02, e12, e23], [e03, e23, e13]]
-
-def generate_two(a, b, c, d):
-    (e0, e1, e2, e3) = (((0, 1), a, 0), ((0, 1), b, 1), ((0, 1), c, 2), ((0, 1), d, 3))
-    return [[e0, e1, e2, e3], [e3, e2, e1, e0]]
-
-def generate_all_sign_two(a, b, c, d):
-    return [generate_two(a, b, -c, -d),
-        generate_two(a, -b, c, -d),
-        generate_two(a, -b, -c, d),
-        generate_two(-a, b, c, -d),
-        generate_two(-a, b, -c, d),
-        generate_two(-a, -b, c, d)]
-
 def format_mathematica(l):
     return str(l).replace("[", "{").replace("]", "}")
 
@@ -47,7 +30,7 @@ def check_all_parity(g):
     return out
 
 #k must be at least 4
-def generate_up_to_k(g, parities, k):
+def generate_up_to_k(g, parities, k, predicate=lambda _: True):
     edges = []
     for v in g:
         for e in v:
@@ -57,24 +40,28 @@ def generate_up_to_k(g, parities, k):
     parities = [x + k - 2 for x in parities]
 
     def valid():
-        if parities[-1] < 3: #skip mirror images so no need to check negative values on the last element 
-            return False
-        for i in range(len(parities) - 1):
-            if (-3 < parities[i] < 3):
-                parities[i] -= 2
-                return valid()
-            if (parities[i] < -k):
-                parities[i] = k - parities[i] % 2
-                parities[i + 1] -= 2
-                return valid()
-            
-        count = 0
-        for p in parities:
-            count += 1 if p < 0 else 0
-        if (count < 2 or len(parities) - count < 2):
-            parities[0] -= 2
-            return valid()
-        return True
+        while True:
+            if parities[-1] < 3: #skip mirror images so no need to check negative values on the last element 
+                return False
+            for i in range(len(parities) - 1):
+                if (-3 < parities[i] < 3):
+                    parities[i] -= 2
+                    break
+                if (parities[i] < -k):
+                    parities[i] = k - parities[i] % 2
+                    parities[i + 1] -= 2
+                    break
+            else:
+                count = 0
+                for p in parities:
+                    count += 1 if p < 0 else 0
+                if (count < 2 or len(parities) - count < 2):
+                    parities[0] -= 2
+                    continue
+                if not predicate(parities):
+                    parities[0] -= 2
+                    continue
+                return True
 
 
     out = []
@@ -91,7 +78,7 @@ def generate_up_to_k(g, parities, k):
     
     return out
     
-def generate_up_to_k_with_sign(g, parities, k):
+def generate_up_to_k_with_sign(g, parities, k, predicate=lambda _: True):
     edges = []
     for v in g:
         for e in v:
@@ -100,28 +87,34 @@ def generate_up_to_k_with_sign(g, parities, k):
     signs = [1 if x > 0 else -1 for x in parities]
     parities = [abs(x) + k - 2 for x in parities]
 
-    def valid():
-        if parities[-1] < 3: 
-            return False
-        for i in range(len(parities) - 1):
-            if (parities[i] < 3):
-                parities[i] = k - parities[i] % 2
-                parities[i + 1] -= 2
-                return valid()
-        return True
+    def prod():
+        return [signs[i] * parities[i] for i in range(len(signs))]
 
+    def valid():
+        while True:
+            if parities[-1] < 3: 
+                return False
+            for i in range(len(parities) - 1):
+                if (parities[i] < 3):
+                    parities[i] = k - parities[i] % 2
+                    parities[i + 1] -= 2
+                    break
+            else: 
+                if not predicate(prod()):
+                    parities[0] -= 2
+                    continue
+                return True
 
     out = []
     while valid():
         temp = []
         for v in g:
             acc = []
-            prod = [signs[i] * parities[i] for i in range(len(signs))]
             for e in v:
                 (a, b, *rest) = e
-                acc.append(((a, b), prod[edges.index(e)], *rest))
+                acc.append(((a, b), prod()[edges.index(e)], *rest))
             temp.append(acc)
-        out.append((prod.copy(), gauss_from_weighted_graph(temp)))
+        out.append((prod(), gauss_from_weighted_graph(temp)))
         parities[0] -= 2
     
     return out
@@ -175,55 +168,3 @@ def gauss_from_weighted_graph(g):
     if len(visited) != 2 * len(edges):
         return None
     return gauss
-
-#(e010,e011,e020,e021,e12) = ((0,1,0), (0, 1, 1), (0, 2, 0), (0, 2, 1), (1, 2))
-#g = [[e010, e011, e020, e021], [e12, e011, e010], [e12, e021, e020]]
-#parities = [-1, -1, -2, 1, 1]
-
-(w0, w1, w2, w3, w4) = (-3, -3, -4, 3, 3)
-(e010,e011,e020,e021,e12) = (((0,1),w0,0),((0,1),w1,1),((0,2),w2,0),((0,2),w3,1),((1, 2),w4))
-print(gauss_from_weighted_graph([[e010, e011, e020, e021], [e12, e011, e010], [e12, e021, e020]]))
-
-#print(check_all_parity(g))
-#ouch = generate_up_to_k_with_sign(g, parities, 18)
-#subprocess.run("pbcopy", text=True, input=format_mathematica([b for (_, b) in ouch]))
-#print(len(ouch))
-
-'''
-print(sorted([ouch[i-1][0] for i in [1, 10, 19, 28, 37, 46, 55, 64, 4225, 4234, 4243, 4252, 4261, 4270, \
-4279, 4288, 4609, 4618, 4627, 4636, 4645, 4654, 4663, 4672, 4673, \
-4682, 4691, 4700, 4709, 4718, 4727, 4736, 8897, 8906, 8915, 8924, \
-8933, 8942, 8951, 8960, 9217, 9226, 9235, 9244, 9253, 9262, 9271, \
-9280, 9281, 9290, 9299, 9308, 9317, 9326, 9335, 9344, 9345, 9354, \
-9363, 9372, 9381, 9390, 9399, 9408, 13569, 13578, 13587, 13596, \
-13605, 13614, 13623, 13632, 13825, 13834, 13843, 13852, 13861, 13870, \
-13879, 13888, 13889, 13898, 13907, 13916, 13925, 13934, 13943, 13952, \
-13953, 13962, 13971, 13980, 13989, 13998, 14007, 14016, 14017, 14026, \
-14035, 14044, 14053, 14062, 14071, 14080, 18241, 18250, 18259, 18268, \
-18277, 18286, 18295, 18304, 18433, 18442, 18451, 18460, 18469, 18478, \
-18487, 18496, 18497, 18506, 18515, 18524, 18533, 18542, 18551, 18560, \
-18561, 18570, 18579, 18588, 18597, 18606, 18615, 18624, 18625, 18634, \
-18643, 18652, 18661, 18670, 18679, 18688, 18689, 18698, 18707, 18716, \
-18725, 18734, 18743, 18752, 22915, 22923, 22929, 22930, 22931, 22940, \
-22949, 22958, 22967, 22976, 23059, 23068, 23077, 23086, 23095, 23104, \
-23123, 23132, 23141, 23150, 23159, 23168, 23187, 23196, 23205, 23214, \
-23223, 23232, 23251, 23260, 23269, 23278, 23287, 23296, 23315, 23324, \
-23333, 23342, 23351, 23360, 23370, 23379, 23388, 23397, 23406, 23415, \
-23424, 27589, 27597, 27605, 27613, 27617, 27618, 27619, 27620, 27621, \
-27630, 27639, 27648, 27685, 27694, 27703, 27712, 27749, 27758, 27767, \
-27776, 27813, 27822, 27831, 27840, 27877, 27886, 27895, 27904, 27941, \
-27950, 27959, 27968, 28005, 28014, 28023, 28032, 28060, 28069, 28078, \
-28087, 28096, 32311, 32320, 32375, 32384, 32439, 32448, 32503, 32512, \
-32567, 32576, 32631, 32640, 32695, 32704, 32750, 32759, 32768]]))
-
-
-
-
-out = {}
-for t in temp:
-    p = []
-    for v in t:
-        p.append((2 - (v % 2)) * (1 if v > 0 else -1))
-    out[tuple(p)] = out.get(tuple(p), []) + [t]
-[print(i, ":", out[i]) for i in out]
-'''
